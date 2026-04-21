25037ef5fcfaf37f89da06f41d607d8d8d45b5ec
MuriloGandini <145728089+MuriloGandini@users.noreply.github.com>
2026-04-21 11:01:45 -0300

    API agora funcionando

--- apps/dndxe/src/routes/roll/+server.ts
+++ apps/dndxe/src/routes/roll/+server.ts
@@ -1,10 +1,10 @@
 import { RandomOrgClient } from "@randomorg/core";
 import { json } from "@sveltejs/kit";
 import type { RequestEvent } from "./$types";
-import {RANDOM_ORG_KEY} from '$env/static/private'
-const random = new RandomOrgClient(RANDOM_ORG_KEY);
+import { RANDOM_API_KEY } from '$env/static/private'
+const random = new RandomOrgClient(RANDOM_API_KEY);
 
-export async function GET({ url }:RequestEvent) {
+export async function GET({ url }: RequestEvent) {
     const quant = Number(url.searchParams.get('quant'));
     const tipo = Number(url.searchParams.get('tipo'));
     const num = await random.generateIntegers(quant, 1, tipo) as number[];
