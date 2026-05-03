65410809c3ffe6067ddec26e49acad542b4cbc8a
MuriloGandini <murilogandinipassarin@gmail.com>
2026-05-02 21:52:58 -0300

    Apagado o dndxe

--- apps/dndxe/src/lib/supabase.server.ts
+++
@@ -1,6 +0,0 @@
-import { createSupabaseClient } from '@xambao/database'
-import { SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private'
-export const supabase = createSupabaseClient(
-    SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE_KEY
-)
